94e7affb76a2cbaba61bc8e9f063cf8e773595be
Alex-Klim93 <melkimelki79@gmail.com>
2025-12-22 21:20:00 +0300

    test

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const port = 3003;
 const server = http.createServer((request, response) => {
   const url = new URL(request.url, `http://${hostname}:${port}`);
   const params = url.searchParams;
+  console.log(url);
+  console.log(url.searchParams);
 
   if (params.has("hello")) {
     const name = params.get("hello");
@@ -47,6 +49,4 @@ const server = http.createServer((request, response) => {
 
 server.listen(port, hostname, () => {
   console.log(`Сервер запущен по адресу http://${hostname}:${port}/`);
-  console.log(url);
-  console.log(url.searchParams);
 });
